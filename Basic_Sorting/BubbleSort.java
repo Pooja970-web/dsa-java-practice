0a71de0638d77c7f223b4248feff83520edfb5e2
Pooja970-web <poojklh15@gmail.com>
2026-01-21 13:21:22 +0530

    BubbleSort code

--- Basic_Sorting/BubbleSort.java
+++ Basic_Sorting/BubbleSort.java
@@ -1,25 +1,31 @@
-package Basic_Sorting;
-public class BubbleSort
- {
-  public static void print(int []arr) {
-    for(int ele:arr){
-      System.out.println(ele+"");
+
+public class BubbleSort {
+
+  public static void print(int[] arr) {
+    for (int ele : arr) {
+      System.out.print(ele + " ");
     }
     System.out.println();
   }
+
   public static void main(String[] args) {
-    int []arr={5,-2,6,7,2,8,7,2};
-    int n=arr.length;
-    System.out.print(arr);
-    for(int j=1;j<=n-1;j++){
-      for(int i=0;i<=n-1;i++){
-        if(arr[i]>arr[i+1]){
-          int temp=arr[i];
-          arr[i]=arr[i+1];
-          arr[i+1]=temp;
+    int[] arr = {5, -2, 6, 7, 2, 8, 7, 2};
+    int n = arr.length;
+
+    System.out.println("Before Sorting:");
+    print(arr);
+
+    for (int j = 1; j <= n - 1; j++) {
+      for (int i = 0; i < n - j; i++) {
+        if (arr[i] > arr[i + 1]) {
+          int temp = arr[i];
+          arr[i] = arr[i + 1];
+          arr[i + 1] = temp;
         }
       }
     }
-    System.out.print(arr);
+
+    System.out.println("After Sorting:");
+    print(arr);
   }
 }
