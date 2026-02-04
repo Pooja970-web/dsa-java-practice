bf915b628bc8791329a46c89a474d1d3dbfc325e
Pooja970-web <poojklh15@gmail.com>
2026-02-04 18:18:29 +0530

    Remove compiled class files and add gitignore

--- Basic_sorting/KthSorted.java
+++ Basic_sorting/KthSorted.java
@@ -5,19 +5,18 @@ public class KthSorted {
 
         for (int i = 0; i < k; i++) {
             int min = Integer.MAX_VALUE;
-            int minIndex = -1;
+            int mindex = -1;
 
             for (int j = i; j < n; j++) {
                 if (arr[j] < min) {
                     min = arr[j];
-                    minIndex = j;
+                    mindex = j;
                 }
             }
 
-            // swap
             int temp = arr[i];
-            arr[i] = arr[minIndex];
-            arr[minIndex] = temp;
+            arr[i] = arr[mindex];
+            arr[mindex] = temp;
         }
 
         return arr[k - 1];
